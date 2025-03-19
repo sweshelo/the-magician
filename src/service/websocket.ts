65cf7ef74edc416b6beab452b11e12184f7170e7
sweshelo <sweshelo@gmail.com>
2025-03-19 16:15:36 +0900

    型定義を外部モジュールに

--- src/service/websocket.ts
+++ src/service/websocket.ts
@@ -1,10 +1,11 @@
-import { Message, RequestPayload } from "@/type/message";
+import { Message } from "@/submodule/suit/types/message/message";
+import { RequestPayload } from "@/submodule/suit/types/message/payload/base";
 import EventEmitter from "events";
 
-class WebSocketService extends EventEmitter{
+class WebSocketService extends EventEmitter {
   private socket: WebSocket;
 
-  constructor(url: string){
+  constructor(url: string) {
     super()
     this.socket = new WebSocket(url);
 
@@ -20,24 +21,25 @@ class WebSocketService extends EventEmitter{
 
   // メッセージをサーバに送る
   // 主にゲーム内で利用
-  send(message: Message){
+  send(message: Message) {
     this.socket.send(JSON.stringify(message));
   }
 
   // あるメッセージに対してサーバ側の応答を待たす
   // 主にゲーム外で利用
-  async request<T extends RequestPayload, R extends RequestPayload>(message: Message<T>): Promise<Message<R>>{
+  async request<T extends RequestPayload, R extends RequestPayload>(message: Message<T>): Promise<Message<R>> {
     this.socket.send(JSON.stringify(message));
 
     return new Promise((resolve, reject) => {
       const handler = (e: MessageEvent) => {
-        try{
-          const response = JSON.parse(e.data) as Message;
-          if('requestId' in response.payload && response.payload.requestId === message.payload.requestId){
+        try {
+          const response = JSON.parse(e.data) as Message<R>;
+          const { payload } = response;
+          if ('requestId' in payload && payload.requestId === payload.requestId) {
             this.socket.removeEventListener('message', handler)
-            resolve(response as Message<R>)
+            resolve(response)
           }
-        } catch(e) {
+        } catch (e) {
           this.socket.removeEventListener('message', handler)
           reject(e)
         }
