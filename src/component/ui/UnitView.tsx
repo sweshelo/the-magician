1880e883cdec8c9f149fe809d3b58d2c6a6b487d
sweshelo <sweshelo@gmail.com>
2025-04-01 23:19:40 +0900

    Unit色設定

--- src/component/ui/UnitView.tsx
+++ src/component/ui/UnitView.tsx
@@ -1,15 +1,24 @@
 import { IUnit } from "@/submodule/suit/types";
 import { BPView } from "./BPView";
 import { UnitIconView } from "./UnitIconView";
+import catalog from "@/submodule/suit/catalog/catalog";
 
 interface UnitViewProps {
   unit: IUnit
 }
 
 export const UnitView = ({ unit }: UnitViewProps) => {
+  const color: string = ({
+    1: 'orangered',
+    2: 'gold',
+    3: 'royalblue',
+    4: 'mediumseagreen',
+    5: 'darkviolet',
+  })[catalog.get(unit.catalogId)?.color ?? 0] ?? ''
+
   return (
     <div className="flex flex-col items-center">
-      <UnitIconView color={'green'} image={`/image/card/full/${unit.catalogId}.jpg`} />
+      <UnitIconView color={color} image={`/image/card/full/${unit.catalogId}.jpg`} />
       <div className="-mt-2">
         <BPView bp={unit.bp} lv={unit.lv}/>
       </div>
