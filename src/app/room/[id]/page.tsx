ddcb2381713bde73274885484a279146c6df5268
sweshelo <sweshelo@gmail.com>
2025-04-20 10:18:06 +0900

    unitselectionprovider 移動

--- src/app/room/[id]/page.tsx
+++ src/app/room/[id]/page.tsx
@@ -1,14 +1,11 @@
 import { Game } from '@/feature/Game';
-import { UnitSelectionProvider } from '@/hooks/unit-selection';
 import { GameContextProvider } from '@/hooks/game/GameContextProvider';
 
 export default async function Page({ params }: { params: Promise<{ id: string }> }) {
   const { id } = await params;
   return (
     <GameContextProvider>
-      <UnitSelectionProvider>
-        <Game id={id} />
-      </UnitSelectionProvider>
+      <Game id={id} />
     </GameContextProvider>
   );
 }
