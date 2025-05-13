ff9a4a36b55e131b963dfeb5f05ab9005e949300
sweshelo <sweshelo@gmail.com>
2025-05-13 23:07:40 +0900

    metadata修正

--- src/app/entrance/page.tsx
+++ src/app/entrance/page.tsx
@@ -4,7 +4,7 @@ import { Metadata } from 'next';
 import Link from 'next/link';
 
 export const metadata: Metadata = {
-  title: 'entrance',
+  title: 'Entrance',
 };
 
 export default function Page() {
