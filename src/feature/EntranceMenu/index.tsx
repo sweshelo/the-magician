1352901c59d419aa744e25aa6e1701efc23a4427
sweshelo <sweshelo@gmail.com>
2026-01-30 14:37:48 +0900

    add message

--- src/feature/EntranceMenu/index.tsx
+++ src/feature/EntranceMenu/index.tsx
@@ -5,7 +5,7 @@ import { MigrationBanner } from './MigrationBanner';
 
 export const EntranceMenu = () => {
   return (
-    <div className="space-y-6 p-6 rounded-lg max-w-lg mx-auto shadow-md">
+    <div className="space-y-6 rounded-lg max-w-lg mx-auto shadow-md">
       <div className="text-center">
         <h2 className="text-2xl font-bold text-black mb-4">ゲーム設定</h2>
       </div>
